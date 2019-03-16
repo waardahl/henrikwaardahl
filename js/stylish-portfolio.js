01486817c03b3dec32dd09a6f0e7001d2f43fd13
Henrik Waardahl <Hwaa@kth.se>
2019-03-16 23:06:08 +0100

    lotta fixes

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -62,3 +62,24 @@ var onMapClickHandler = function(event) {
 }
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
+
+
+//next modal change
+$("div[id^='myModal']").each(function(){
+  
+  var currentModal = $(this);
+  
+  //click next
+  currentModal.find('.btn-next').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
+  });
+  
+  //click prev
+  currentModal.find('.btn-prev').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
+  });
+
+});
+
